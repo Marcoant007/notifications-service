8e371b0481537e71ea4116fc6828011b34a16d86
Marcoant007 <marcoantnovo@gmail.com>
2022-12-14 00:02:18 -0300

    finish first day

---
+++ src/create-notification-body.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+
+import { IsNotEmpty, IsUUID, Length } from "@nestjs/class-validator";
+
+
+export class CreateNotificationBody {
+
+    @IsNotEmpty()
+    @IsUUID()
+    recipientId: string;
+
+    @IsNotEmpty()
+    @Length(5, 240)
+    content:string;
+
+    @IsNotEmpty()
+    category: string;
+
+
+}
